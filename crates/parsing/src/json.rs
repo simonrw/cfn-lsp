f4208b49642dd303969eeda3eb4fe5a34096d5a0
Simon Walker <s.r.walker101@googlemail.com>
2025-07-14 17:04:54 +0100

    Set up parsing

---
+++ crates/parsing/src/json.rs
@@ -0,0 +1,5 @@
+use crate::Template;
+
+pub(crate) fn parse(content: &str) -> std::result::Result<Template, crate::ParsingError> {
+    todo!()
+}
--- crates/parsing/src/lib.rs
+++ crates/parsing/src/lib.rs
@@ -1,14 +1,47 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::io::Read;
+
+use thiserror::Error;
+mod json;
+mod types;
+mod yaml;
+
+pub use types::*;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("could not detect file type")]
+    NoFileType,
+
+    #[error("parsing error: {0}")]
+    Parsing(#[from] ParsingError),
+}
+#[derive(Error, Debug)]
+pub enum ParsingError {}
+
+pub type Result<T> = std::result::Result<T, Error>;
+
+enum FileType {
+    Json,
+    Yaml,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn detect_file_type(content: &str) -> Option<FileType> {
+    // TODO
+    Some(FileType::Yaml)
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn parse(content: &str) -> Result<Template> {
+    match detect_file_type(content) {
+        Some(FileType::Json) => json::parse(content).map_err(Error::Parsing),
+        Some(FileType::Yaml) => yaml::parse(content).map_err(Error::Parsing),
+        None => Err(Error::NoFileType),
     }
+}
+
+pub fn parse_from(mut reader: impl Read) -> Result<Template> {
+    let mut content = String::new();
+    reader.read_to_string(&mut content).map_err(Error::Io)?;
+    parse(&content)
 }
