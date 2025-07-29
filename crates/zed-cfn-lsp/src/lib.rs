8b5d548790c2372dd179723227982e6d9f2e616e
Simon Walker <s.r.walker101@googlemail.com>
2025-07-29 20:59:33 +0100

    Set up language server configuration

--- crates/zed-cfn-lsp/src/lib.rs
+++ crates/zed-cfn-lsp/src/lib.rs
@@ -9,6 +9,30 @@ impl zed::Extension for CloudFormationLanguageServer {
     {
         todo!()
     }
+
+    fn language_server_command(
+        &mut self,
+        _language_server_id: &zed::LanguageServerId,
+        _worktree: &zed::Worktree,
+    ) -> zed::Result<zed::Command> {
+        Ok(zed::Command {
+            command: get_path_to_language_server_executable()?,
+            args: get_args_for_language_server()?,
+            env: get_env_for_language_server()?,
+        })
+    }
+}
+
+fn get_env_for_language_server() -> zed::Result<Vec<(String, String)>> {
+    todo!()
+}
+
+fn get_args_for_language_server() -> zed::Result<Vec<String>> {
+    todo!()
+}
+
+fn get_path_to_language_server_executable() -> zed::Result<String> {
+    todo!()
 }
 
 zed::register_extension!(CloudFormationLanguageServer);
