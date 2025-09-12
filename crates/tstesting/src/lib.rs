c1a1b419117ec12e8f4cc92f4a1f874a0e6b3119
Simon Walker <s.r.walker101@googlemail.com>
2025-09-12 22:58:45 +0100

    WIP: start to build out parsing

--- crates/tstesting/src/lib.rs
+++ crates/tstesting/src/lib.rs
@@ -1,14 +1,70 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct Position {
+    line: u32,
+    column: u32,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+struct Span {
+    start: Position,
+    end: Position,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct SpannedNode {
+    span: Span,
+    node: Node,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+enum Node {
+    Sub {
+        template: String,
+        arguments: Option<HashMap<String, Node>>,
+    },
+}
+
+fn parse(input: &str) -> Result<Vec<SpannedNode>, Box<dyn std::error::Error>> {
+    Ok(vec![])
 }
 
 #[cfg(test)]
 mod tests {
+    use std::path::Path;
+
     use super::*;
 
+    fn template_string(stub: impl AsRef<Path>) -> Result<String, Box<dyn std::error::Error>> {
+        let path = dbg!(std::env::current_dir()?)
+            .join("..")
+            .join("..")
+            .join("crates")
+            .join("cfn-lsp")
+            .join("testdata")
+            .join(stub.as_ref());
+        let content = std::fs::read_to_string(path)?;
+        Ok(content)
+    }
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn it_works() -> Result<(), Box<dyn std::error::Error>> {
+        let sub_inline = template_string("sub_inline.json")?;
+        let intrinsics = parse(&sub_inline)?;
+        assert_eq!(
+            intrinsics,
+            [SpannedNode {
+                span: Span {
+                    start: Position { line: 0, column: 0 },
+                    end: Position { line: 0, column: 0 },
+                },
+                node: Node::Sub {
+                    template: "${MyParameter}".to_string(),
+                    arguments: None,
+                },
+            }]
+        );
+        Ok(())
     }
 }
